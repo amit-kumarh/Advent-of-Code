7f2550b30b7d8f676619a44f24de05bcb085117f
Amit Kumar <amitkh@gmail.com>
2023-12-01 19:08:38 +1100

    lint

--- 2023/1/1.py
+++ 2023/1/1.py
@@ -1,7 +1,7 @@
 from aocd import get_data
 import re
 
-content = [line.strip() for line in get_data(day=1, year=2023).split('\n')]
+content = [line.strip() for line in get_data(day=1, year=2023).split("\n")]
 d = {
     "one": "1",
     "two": "2",
@@ -9,15 +9,15 @@ d = {
     "four": "4",
     "five": "5",
     "six": "6",
-    "seven":"7",
-    "eight":"8",
-    "nine":"9",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
 }
 ans = 0
 for line in content:
     m = re.findall(r"one|two|three|four|five|six|seven|eight|nine|\d", line)
     first = d.get(m[0], m[0])
     second = d.get(m[-1], m[-1])
-    ans += int(first+second)
+    ans += int(first + second)
 
 print(ans)
