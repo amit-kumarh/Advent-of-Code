a80e11da39f19f2162eb20c4f6e63f48e0f50193
Amit Kumar <amitkh@gmail.com>
2023-12-04 13:26:14 +1100

    better 2023 day 3

--- 2023/3/3.py
+++ 2023/3/3.py
@@ -1,77 +1,38 @@
 import sys
-import itertools
-from math import prod
+import math
+import collections
 
 with open(sys.argv[1]) as f:
-    c = [l.strip() for l in f.read().strip().split('\n')]
+    c = [l.strip() for l in f.read().split("\n")]
 
-DIR = list(itertools.permutations([-1, 0, 1], 2) ) + [(1, 1), (-1, -1)]
-def check_char(x, y):
-    for dx, dy in DIR:
-        row = x + dx
-        col = y + dy
-        if not (0 <= row < len(c) and 0 <= col < len(c[0])):
-            continue
+DIR = [complex(dx, dy) for dx in range(-1, 2) for dy in range(-1, 2)]
+grid = {
+    complex(row, col): char
+    for row, line in enumerate(c)
+    for col, char in enumerate(line)
+}
 
-        if c[row][col] != "." and not c[row][col].isdigit():
-            return True
-
-    return False
-
-p1 = j = 0
-for (i, _) in enumerate(c):
-    j = 0
-    while j < len(c[0]):
-        num = ""
-        good = False
-        while j < len(c[0]) and c[i][j].isdigit():
-            if check_char(i, j):
-                good = True
-            num += c[i][j]
-            j += 1
-
-        if num != "" and good:
-            p1 += int(num)
-
-        j += 1
+p1 = 0
+stars = collections.defaultdict(list)
+for row in range(len(c)):
+    num, part, adj_stars = 0, False, set()
+    for col in range(len(c[row]) + 1):
+        char = grid.get(coord := complex(row, col), ".")
+        if char.isdigit():
+            num = num * 10 + int(char)
+            for d in DIR:
+                char = grid.get(coord + d, ".")
+                part |=  char not in "1234567890."
+                if char == "*":
+                    adj_stars.add(coord+d)
+        else:
+            if num and part:
+                p1 += num
+                for s in adj_stars:
+                    stars[s].append(num)
+            num, part, adj_stars = 0, False, set()
 
 print(p1)
-
-p2 = j = 0
-for (i, _) in enumerate(c):
-    j = 0
-    while j < len(c[0]):
-        if c[i][j] == '*':
-            nums = set()
-            for dx, dy in DIR:
-                row = i + dx
-                col = j + dy
-                if not (0 <= row < len(c) and 0 <= col < len(c[0])):
-                    continue
-
-                if c[row][col].isdigit():
-                    numr = ""
-                    while col < len(c[0]) and c[row][col].isdigit():
-                        numr += c[row][col]
-                        col += 1
-
-                    numl = ""
-                    row = i + dx
-                    col = j + dy - 1
-                    while col < len(c[0]) and c[row][col].isdigit():
-                        numl += c[row][col]
-                        col -= 1
-
-                    num = ''.join(list(reversed(numl))) + numr
-                    if num != "":
-                        nums.add(int(num))
-
-            if len(nums) == 2:
-                p2 += prod(nums)
-
-        j += 1
-
+p2 = sum(math.prod(i for i in l) for l in stars.values() if len(l) == 2)
 print(p2)
 
-
-        
