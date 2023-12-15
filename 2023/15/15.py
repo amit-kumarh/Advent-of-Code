efb0877681ffa0d128daf9f1a7da4500efd8a894
Amit Kumar <amitkh@gmail.com>
2023-12-15 11:50:58 +0530

    day 15 2023

---
+++ 2023/15/15.py
@@ -0,0 +1,17 @@
+import sys
+import functools
+
+with open(sys.argv[1]) as f:
+    contents = f.read().strip().split(',')
+
+def hash(s):
+    return functools.reduce(lambda x, y: (x + ord(y)) * 17 % 256, s, 0)
+
+boxes = [dict() for _ in range(256)]
+for inst in contents:
+    match inst.split('='):
+        case [label, rest]: boxes[hash(label)][label] = int(rest)
+        case [inst]: boxes[hash(label := inst[:-1])].pop(label, "nothing")
+
+print(sum(hash(inst) for inst in contents))
+print(sum((i+1) * (j+1) * power for i, box in enumerate(boxes) for j, power in enumerate(box.values())))
