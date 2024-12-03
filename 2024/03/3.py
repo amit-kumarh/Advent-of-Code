737ceaf88772a6adca0fd24c91e037d9a970fa63
Amit Kumar <amitkh@gmail.com>
2024-12-03 01:13:22 -0500

    findall -> finditer

--- 2024/03/3.py
+++ 2024/03/3.py
@@ -4,17 +4,16 @@ import re
 with open(sys.argv[1]) as f:
     contents = f.read()
 
-inst = re.findall(r'mul\(\d\d?\d?,\d\d?\d?\)|do\(\)|don\'t\(\)', contents)
+inst = re.finditer(r'mul\((\d\d?\d?),(\d\d?\d?)\)|do\(\)|don\'t\(\)', contents)
 p1 = p2 = 0
 on = True
 for i in inst:
-    if i.startswith( "mul"):
-        nums = [int(x) for x in re.findall(r'\d+', i)]
-        p1 += nums[0] * nums[1]
-        p2 += nums[0] * nums[1] * on
-    elif i.startswith("don't"):
+    if i[0].startswith( "mul"):
+        p1 += int(i[1]) * int(i[2])
+        p2 += int(i[1]) * int(i[2]) * on
+    elif i[0].startswith("don't"):
         on = False
-    elif i.startswith("do"):
+    elif i[0].startswith("do"):
         on = True
 
 print(p1, p2, sep='\n')
